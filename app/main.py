718aae95c16bd8067972691610269d67debea155
Utkarsh Bajpai <76493896+0blackskull@users.noreply.github.com>
2025-10-28 21:19:53 +0530

    echo hello testcase works

--- app/main.py
+++ app/main.py
@@ -5,6 +5,120 @@ import types
 sel = selectors.DefaultSelector()
 
 """
+Decode incoming RESP payload (array of bulk strings)
+returns array and remaining buffer
+"""
+class RESParser:
+    def __init__(self) -> None:
+        # type to arr_len to bulk_len to bulk_data
+        self.state  = "type"
+        self.pos = 0
+        # binary received 
+        self.buf = bytearray() # mutable byte array
+        # processed
+        self.args = []
+        self.expected_args = 0
+        self.bulk_len = 0
+        self.bulk_data = b""
+
+    def _readline(self):
+        end_idx = self.buf.find(b"\r\n")
+        if end_idx < 0:
+            return None
+        
+        line = self.buf[self.pos : end_idx]
+        self.pos = end_idx + 2
+        return line
+
+    def parse(self, data):
+        self.buf += data
+
+        while True:
+            # Prevent overrun
+            if self.pos >= len(self.buf):
+                break
+
+            token = self.buf[self.pos : self.pos + 1]
+
+            # if token == b"\n":
+            #     self.pos += 1
+            #     continue
+
+            if self.state == "type":
+                # Array start detected
+                if token == b"*":
+                    # Expect array length next
+                    self.state = "arr_len"
+                # Bulk string start detected
+                elif token == b"$":
+                    self.state = "bulk_len"
+                else:
+                    raise ValueError(f"Unsupported token found: {token!r}") # !r for raw string
+
+                self.pos += 1
+
+            elif self.state == "bulk_data":
+
+                if len(self.bulk_data) == self.bulk_len:
+                    if token != b"\r" and token != b"\n":
+                        raise ValueError(f"Malformed payload: {self.bulk_data!r}")
+                    
+                    self.args.append(self.bulk_data.decode('utf-8'))
+
+                    # Ready for next unit
+                    self.state = "type"
+                    self.bulk_len = 0
+                    self.bulk_data = b""
+                    
+                else:
+                    self.bulk_data += token
+                
+                self.pos += 1
+
+
+            elif self.state == "bulk_len":
+                # Bulk string length reading complete
+                if token == b"\r" or token == b"\n":
+                    self.state = "bulk_data"
+                # Update expected number of args
+                else:
+                    self.bulk_len = self.bulk_len * 10 + int(token.decode())
+
+                self.pos += 1
+
+            
+            elif self.state == "arr_len":
+                
+                # Array length (number of args) reading complete
+                if token == b"\r":
+                    self.state = "type"
+                # Update expected number of args
+                else:
+                    self.expected_args = self.expected_args * 10 + int(token.decode())
+                    print(int(token.decode()))
+
+                self.pos += 1
+
+            print(f"Token {token} ; State {self.state} ; Pos {self.pos} ; Expected args {self.expected_args} ; Bulk len {self.bulk_len}")
+            print("args", self.args) # prints ["ECHO", "HELLO"]
+
+
+            
+parser = RESParser()
+
+chunks = [
+    b"*2\r\n$4\r\nE",     # arrives first
+    b"CHO\r\n$5\r\nhe",   # second
+    b"llo\r\n"             # last
+]
+
+for chunk in chunks:
+    parser.parse(chunk)
+    
+
+
+
+"""
 Accept connection on a listening socket ready for read
 """
 def accept_connection(sock):
@@ -13,7 +127,7 @@ def accept_connection(sock):
     conn.setblocking(False)
     
     try:
-        data = types.SimpleNamespace(inb=b"", outb=b"", addr=addr)
+        data = types.SimpleNamespace(parser=RESParser, outb=b"", addr=addr)
         io_events = selectors.EVENT_READ | selectors.EVENT_WRITE
         sel.register(conn, io_events, data)
     except KeyError:
@@ -70,5 +184,5 @@ def main():
                 service_connection(key, mask)
 
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
